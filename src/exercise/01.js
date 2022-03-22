d65c2dee443f214cac8badd78acbd6ddb1daf66d
Abdul <abdul.k.khan75@gmail.com>
2022-03-21 22:22:14 -0400

    finish module 1 extra credit 1

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,27 +3,26 @@
 
 import * as React from 'react'
 
-const countReducer = (state, action) => {
-  switch (action.type) {
-    case 'increment':
-      return {count: state.count + 1};
-    case 'decrement':
-      return {count: state.count - 1};
-    default:
-      throw new Error();
-  }
+// (prevState, nextState)
+// state, action
+const countReducer = (state, newState) => {
+
+  console.log('state', state)
+  console.log('newState', newState)
+  return newState + 1
+
 }
 
-function Counter({initialCount = 0, step = 1}) {
+function Counter({ initialCount = 0, step = 1 }) {
 
   // const [state, dispatch] = useReducer(reducer, initialArg, init);
-  // The 1st argument is called "state" - the current value of count
-  // The 2nd argument is called "newState" - the value passed to setCount
-  const [count, dispatchChangeCount] = React.useReducer(countReducer, { count: initialCount })
+  const [count, changeCount] = React.useReducer(countReducer, initialCount)
+
   const increment = () => {
-    return dispatchChangeCount({type: 'increment'})
+    changeCount(count)
   }
-  return <button onClick={increment}>{count.count}</button>
+
+  return <button onClick={ increment }>{ count }</button>
 }
 
 function App() {
